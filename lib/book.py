4e7ae73aa639ad387544802cea99b2e95419067c
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-12-08 12:55:30 +0300

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,11 @@
 
 class Book:
     pass
-    
-        
+    def __init__(self,title,page_count):
+        self.title = title
+        if not isinstance(page_count,int):
+            print("page_count must be an integer")
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,16 @@
 
 class Shoe:
     pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+
+    def set_size(self,size):
+        if not isinstance(size,int):
+            print("size must be an integer")
+        else:    
+            self.size = size   
+     
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
