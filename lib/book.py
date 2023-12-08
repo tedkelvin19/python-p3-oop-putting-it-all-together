d292866df0b8b4287ee06af3d112dd0c9fa60f24
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-12-08 13:09:08 +0300

    Completed lab

--- lib/book.py
+++ lib/book.py
@@ -2,11 +2,20 @@
 
 class Book:
     pass
-    def __init__(self,title,page_count):
-        self.title = title
-        if not isinstance(page_count,int):
+    def __init__(self, title, page_count):
+        self.title=title
+        self.page_count=page_count
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
             print("page_count must be an integer")
-        self.page_count = page_count
+        else:
+            self._page_count=page_count
+
+    page_count=property(get_page_count, set_page_count)
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,16 +2,21 @@
 
 class Shoe:
     pass
-    def __init__(self,brand,size):
-        self.brand = brand
-        self.size = size
+    def __init__(self, brand, size):
+        self.brand=brand
+        self.size=size
 
-    def set_size(self,size):
-        if not isinstance(size,int):
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if not isinstance(size, int):
             print("size must be an integer")
-        else:    
-            self.size = size   
-     
-    def cobble(self):
+        else:
+            self._size=size
+
+    size=property(get_size, set_size)
+
+    def cobble(self, condition="New"):
+        self.condition=condition
         print("Your shoe is as good as new!")
-        self.condition = "New"
